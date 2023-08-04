67d25c428432a5be704bf48db2bce73112a1d1cd
Arslan <arslan99111@gmail.com>
2023-08-04 09:23:46 +0500

    First page animation

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ function circleMouseFollower() {
 }
 
 circleMouseFollower();
+
+function firstPageAnimation() {
+  var tl = gsap.timeline();
+
+  tl.from("#nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+    .to(".boundingelem", {
+      y: "0",
+      duration: 1.5,
+      stagger: 0.2,
+      ease: Expo.easeInOut,
+    })
+    .from("#herofooter", {
+      y: "-10",
+      opacity: 0,
+      duration: 1,
+      ease: Expo.easeInOut,
+    });
+}
+
+firstPageAnimation();
